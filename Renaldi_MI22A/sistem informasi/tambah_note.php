d494bac3afb326cd45060f54d0aeff7821ffe8b6
MasayuNisaAtika <masayunisaatika19@gmail.com>
2025-01-10 07:39:20 +0700

    update peringatan

--- Renaldi_MI22A/sistem informasi/tambah_note.php
+++ Renaldi_MI22A/sistem informasi/tambah_note.php
@@ -1,33 +1,42 @@
 <?php
 include 'db.php';
 
-if (isset($_POST['tambah'])) {
-    $nama_barang = $_POST['Nama_Barang'];
-    $jumlah = $_POST['Jumlah'];
-    $harga_per_unit = $_POST['Harga_Per_Unit'];
-    $jenis_transaksi = $_POST['jenis_transaksi'];
-    $jenis_layanan = $_POST['Jenis_Layanan'];
-    $nama_toko = $_POST['nama_toko']; // ID pelanggan yang akan dipilih
-    $keterangan = $_POST['Keterangan'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $jumlah_barang = intval($_POST['Jumlah']); // Jumlah barang yang akan dikeluarkan
+    $harga_satuan = floatval($_POST['Harga_Per_Unit']); // Harga satuan
+    $keterangan = $_POST['Keterangan']; // Keterangan transaksi
 
-    // Menghitung total harga
-    $total_harga = $jumlah * $harga_per_unit;
+    // Hitung sisa stok saat ini
+    $query_total_stok = "SELECT SUM(jumlah) AS total_stok FROM m_stok";
+    $result_total_stok = mysqli_query($koneksi, $query_total_stok);
+    $total_stok = 0;
+    if ($row_stok = mysqli_fetch_assoc($result_total_stok)) {
+        $total_stok = $row_stok['total_stok'];
+    }
 
-    // Query untuk menambahkan data transaksi ke tabel catatan_transaksi
-    $query = "
-        INSERT INTO t_catatan_transaksi (nama_barang, jumlah, harga_per_unit, total_harga, jenis_transaksi, jenis_layanan, nama_toko, keterangan)
-        VALUES ('$nama_barang', '$jumlah', '$harga_per_unit', '$total_harga', '$jenis_transaksi', '$jenis_layanan', '$nama_toko', '$keterangan')
-    ";
+    $query_total_dikeluarkan = "SELECT SUM(jumlah) AS total_dikeluarkan FROM t_catatan_transaksi";
+    $result_total_dikeluarkan = mysqli_query($koneksi, $query_total_dikeluarkan);
+    $total_dikeluarkan = 0;
+    if ($row_transaksi = mysqli_fetch_assoc($result_total_dikeluarkan)) {
+        $total_dikeluarkan = $row_transaksi['total_dikeluarkan'];
+    }
 
+    $sisa_stok = $total_stok - $total_dikeluarkan;
+
+    // Validasi: cek apakah stok mencukupi
+    if ($jumlah_barang > $sisa_stok) {
+        echo "<script>alert('Stok tidak mencukupi! Sisa stok hanya $sisa_stok.');</script>";
+        echo "<script>window.location.href='catatan_transaksi.php';</script>";
+        exit; // Hentikan proses jika stok tidak mencukupi
+    }
+
+    // Jika stok mencukupi, lanjutkan proses transaksi
+    $query = "INSERT INTO t_catatan_transaksi (jumlah, harga_satuan, keterangan) VALUES ($jumlah_barang, $harga_satuan, '$keterangan')";
     if (mysqli_query($koneksi, $query)) {
-        echo "<script>
-                alert('Data berhasil ditambahkan!');
-                window.location.href = 'catatan_transaksi.php';
-              </script>";
+        echo "<script>alert('Transaksi berhasil ditambahkan!');</script>";
+        echo "<script>window.location.href='dashboard.php';</script>";
     } else {
-        echo "<script>
-                alert('Gagal menambahkan data.');
-              </script>";
+        echo "Error: " . mysqli_error($koneksi);
     }
 }
 ?>
